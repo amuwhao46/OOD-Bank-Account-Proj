3769ab6c999a1fd7c87ff5309e48e2da12f8d37b
Oke Amuwha <amuwhao46@gmail.com>
2021-04-12 13:39:21 -0400

    Initial Commit

---
+++ CSC project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSC project
+//
+//  Created by Oke-Oghene Amuwha on 4/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
